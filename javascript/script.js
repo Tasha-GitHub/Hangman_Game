d5f41eee8916739b85fcffb878f466d7a9bb8816
Tasha Casagni <Tasha@Tashas-MacBook-Pro.local>
2016-10-31 19:14:35 -0500

    added gitignore and arranged files, started JS build

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+var thronesList = ["House Baratheon", "House Bolton", "House Florent", "House Lannister", "House Martell","House Redwyne", "House Stark", "House Umber", "House Targaryen","House Tully"];
+
+
+console.log(Math.floor(Math.random()* thronesList.length + 1));
+console.log("thrones list lenght: " + thronesList.length);
+
+function hangmanSelector() {
+	console.log(Math.random());
+
+}
