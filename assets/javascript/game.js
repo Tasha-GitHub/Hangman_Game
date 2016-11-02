8144090611f46e08eaf7e9ee51ee3153f093787e
Tasha Casagni <Tasha@Tashas-MacBook-Pro.local>
2016-11-01 22:45:01 -0500

    updating js file to stop duplicates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,7 +26,7 @@ function hangmanSelector() {
 	var hangmanIndex = thronesList[selector];
 	/*console.log(hangmanIndex);
 	console.log(selector);*/
-	/* parsing out the word that was chosen into characters and putting into a string*/
+/* parsing out the word that was chosen into characters and putting into a string*/
 	for (var i =0 ; i < hangmanIndex.length ; i++ ){
 		 chosenWordArray.push(hangmanIndex.charAt(i));
 		 templateArray.push(hangmanIndex.charAt(i))
@@ -46,23 +46,38 @@ document.onkeyup = function(event) {
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 	console.log("user guesses the letter: " + userGuess);
 
-/* looking to see if user entry is in the current array and counting remaining guesses*/ 
-	/*console.log(chosenWordArray.indexOf(userGuess));*/
+/* looking to see if user entry is in the current array and counting remaining guesses --edit this comment*/ 
 	var isPartOfWord = chosenWordArray.indexOf(userGuess);
-	if(isPartOfWord >= 0){
-		console.log(" this is part of hangan word");
-		templateArray.pop();
-		console.log("the new template array is " + templateArray);
-	} else {
-		console.log("this is not in word");
-		remainingGuesses--;
-	}
+
+	console.log("what isvariable is part of word = "+ isPartOfWord);
+/*checks to see if and element in the chosen word appears more than once*/
+	    var count = 0;
+	    for (var i = 0; i < chosenWordArray.length; i++) {
+	        if (chosenWordArray[i] === userGuess) {
+	            count++;
+	        }
+	    }
+	    console.log(chosenWordArray.length);
+	console.log("the count of items in an array is " + count);
+
+
+/*this will check and see if the guess is part of the main word & account for duplicate letters ---edit this comment */
+		if(isPartOfWord >= 0){
+			for (var i =0; i < count; i++){
+			console.log(" this is part of hangan word");
+			templateArray.pop();
+			console.log("the new template array is " + templateArray);
+			}
+		} else {
+			console.log("this is not in word");
+			remainingGuesses--;
+		}
 	console.log("guesses remaining: " +remainingGuesses);
 
-/* stops user from putting in the same letter */
+/* stops user from putting in the same letter --------needs work */
 	var isAlreadyTyped = lettersGuessed.indexOf(userGuess);
 	if(isAlreadyTyped > 0){
-		console.log(" you already typed that letter");
+		alert(" you already typed that letter");
 
 	} else {
 		console.log("you have not typed this before");
@@ -79,12 +94,14 @@ document.onkeyup = function(event) {
 	if (remainingGuesses === 0) {
 		userWon = true;
 		gameOver = true;
+		gamesLost++;
 	}
 
 /* determines if game is over and user won*/
 	if (templateArray.length === 0) {
 		userLost = true;
 		gameOver = true;
+		gamesWon++;
 	}
 
 /* if game needs to be reset (player wins or looses)*/
